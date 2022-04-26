3f082d5d1c1601fb8a38e28ac0390cb1340309e7
Fahmi Yufrizal Yusuf <60032856+fahmiyufrizal@users.noreply.github.com>
2022-04-26 21:47:47 +0700

    lmao yeah

---
+++ nxtlvlcosmoo.py
@@ -0,0 +1,40 @@
+# GTA V Inject Save Game 100% 
+# by fahmiyufrizal@2022
+
+#import module
+import os
+from os import path
+import pathlib
+import tkinter as tk
+from tkinter import messagebox
+import shutil
+import webbrowser
+
+#path-basic
+nxtlvl = (r'Launcher_GTAV_Inject.exe')
+savegame = pathlib.Path(r'_GTAV_Data_\savegame\SGTA50015')
+instruksi = (r'_GTAV_Data_\instruksi.txt')
+github = 'https://github.com/fahmiyufrizal'
+
+#path savefile
+protected
+
+#protection
+if not path.exists (nxtlvl):
+	protected
+#def inject
+def Inject100():
+	protected
+		
+#def launchgtav
+def LaunchGTAV():
+	if path.exists('PlayGTAV.exe'):
+		os.system("start "" PlayGTAV.exe")
+	elif path.exists('GTAV.exe'):
+		os.system("start "" GTAV.exe")
+	else:
+		tk.messagebox.showerror('fahmiyufrizal@2022','Tidak dapat menemukan GTAVLauncher.exe maupun GTAV.exe. Mohon letakkan Injector ini dengan benar di dalam folder GTA V!')
+
+#def msgbox
+if path.exists(instruksi):
+	protected
